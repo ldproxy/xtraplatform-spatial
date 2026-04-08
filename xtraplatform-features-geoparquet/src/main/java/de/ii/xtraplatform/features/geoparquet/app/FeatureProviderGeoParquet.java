f28db7def9c261cb108162cf86cd19868cb24d9b
Afeef Neiroukh <aneiroukh@interactive-instruments.de>
2026-04-08 15:57:34 +0200

    Init GeoParquet Feature Provider

---
+++ xtraplatform-features-geoparquet/src/main/java/de/ii/xtraplatform/features/geoparquet/app/FeatureProviderGeoParquet.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2024 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.xtraplatform.features.geoparquet.app;
+
+import dagger.assisted.Assisted;
+import dagger.assisted.AssistedInject;
+import de.ii.xtraplatform.base.domain.resiliency.VolatileRegistry;
+import de.ii.xtraplatform.cache.domain.Cache;
+import de.ii.xtraplatform.cql.domain.Cql;
+import de.ii.xtraplatform.crs.domain.CrsInfo;
+import de.ii.xtraplatform.crs.domain.CrsTransformerFactory;
+import de.ii.xtraplatform.docs.DocDefs;
+import de.ii.xtraplatform.docs.DocStep;
+import de.ii.xtraplatform.docs.DocStep.Step;
+import de.ii.xtraplatform.docs.DocTable;
+import de.ii.xtraplatform.docs.DocTable.ColumnSet;
+import de.ii.xtraplatform.entities.domain.Entity;
+import de.ii.xtraplatform.entities.domain.Entity.SubType;
+import de.ii.xtraplatform.features.domain.ConnectorFactory;
+import de.ii.xtraplatform.features.domain.DecoderFactories;
+import de.ii.xtraplatform.features.domain.FeatureProvider;
+import de.ii.xtraplatform.features.domain.FeatureProviderConnector;
+import de.ii.xtraplatform.features.domain.FeatureProviderDataV2;
+import de.ii.xtraplatform.features.domain.ProviderData;
+import de.ii.xtraplatform.features.domain.ProviderExtensionRegistry;
+import de.ii.xtraplatform.features.sql.domain.FeatureProviderSql;
+import de.ii.xtraplatform.features.sql.domain.FeatureProviderSqlData;
+import de.ii.xtraplatform.features.sql.domain.SqlDbmsAdapters;
+import de.ii.xtraplatform.features.sql.domain.SqlQueryBatch;
+import de.ii.xtraplatform.features.sql.domain.SqlQueryOptions;
+import de.ii.xtraplatform.features.sql.domain.SqlRow;
+import de.ii.xtraplatform.services.domain.Scheduler;
+import de.ii.xtraplatform.streams.domain.Reactive;
+import de.ii.xtraplatform.values.domain.ValueStore;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@DocDefs(
+    tables = {
+      @DocTable(
+          name = "connectionInfo",
+          rows = {
+            @DocStep(type = Step.TAG_REFS, params = "{@ref:connectionInfo}"),
+            @DocStep(type = Step.JSON_PROPERTIES)
+          },
+          columnSet = ColumnSet.JSON_PROPERTIES),
+    })
+@Entity(
+    type = ProviderData.ENTITY_TYPE,
+    subTypes = {
+      @SubType(key = ProviderData.PROVIDER_TYPE_KEY, value = FeatureProvider.PROVIDER_TYPE),
+      @SubType(
+          key = ProviderData.PROVIDER_SUB_TYPE_KEY,
+          value = FeatureProviderGeoParquet.PROVIDER_SUB_TYPE)
+    },
+    data = FeatureProviderSqlData.class)
+public class FeatureProviderGeoParquet extends FeatureProviderSql {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(FeatureProviderGeoParquet.class);
+
+  public static final String ENTITY_SUB_TYPE = "feature/geoparquet";
+  public static final String PROVIDER_SUB_TYPE = "GEOPARQUET";
+
+  @AssistedInject
+  public FeatureProviderGeoParquet(
+      CrsTransformerFactory crsTransformerFactory,
+      CrsInfo crsInfo,
+      Cql cql,
+      ConnectorFactory connectorFactory,
+      SqlDbmsAdapters dbmsAdapters,
+      Reactive reactive,
+      ValueStore valueStore,
+      ProviderExtensionRegistry extensionRegistry,
+      DecoderFactories decoderFactories,
+      VolatileRegistry volatileRegistry,
+      Cache cache,
+      Scheduler scheduler,
+      @Assisted FeatureProviderDataV2 data) {
+    super(
+        crsTransformerFactory,
+        crsInfo,
+        cql,
+        connectorFactory,
+        dbmsAdapters,
+        reactive,
+        valueStore,
+        extensionRegistry,
+        decoderFactories,
+        volatileRegistry,
+        cache,
+        scheduler,
+        data,
+        Map.of());
+  }
+
+  @Override
+  protected boolean onStartup() throws InterruptedException {
+    /*
+    if (!Objects.equals(getData().getConnectionInfo().getDialect(), SqlDbmsAdapterOras.ID)) {
+      LOGGER.error(
+          "Feature provider with id '{}' could not be started: dialect '{}' is not supported",
+          getId(),
+          getData().getConnectionInfo().getDialect());
+      return false;
+    }
+     */
+
+    return super.onStartup();
+  }
+
+  @Override
+  protected FeatureProviderConnector<SqlRow, SqlQueryBatch, SqlQueryOptions> createConnector(
+      String providerSubType, String connectorId) {
+    return super.createConnector(FeatureProviderSql.PROVIDER_SUB_TYPE, connectorId);
+  }
+}
---
+++ xtraplatform-features-geoparquet/src/main/java/de/ii/xtraplatform/features/geoparquet/app/FeatureProviderGeoParquetFactory.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2022 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.xtraplatform.features.geoparquet.app;
+
+import com.github.azahnen.dagger.annotations.AutoBind;
+import dagger.Lazy;
+import dagger.assisted.AssistedFactory;
+import de.ii.xtraplatform.base.domain.LogContext;
+import de.ii.xtraplatform.entities.domain.AbstractEntityFactory;
+import de.ii.xtraplatform.entities.domain.AutoEntityFactory;
+import de.ii.xtraplatform.entities.domain.EntityData;
+import de.ii.xtraplatform.entities.domain.EntityDataBuilder;
+import de.ii.xtraplatform.entities.domain.EntityFactory;
+import de.ii.xtraplatform.entities.domain.PersistentEntity;
+import de.ii.xtraplatform.entities.domain.ValidationResult.MODE;
+import de.ii.xtraplatform.features.domain.ConstantsResolver;
+import de.ii.xtraplatform.features.domain.FeatureProviderDataV2;
+import de.ii.xtraplatform.features.domain.FeatureSchema;
+import de.ii.xtraplatform.features.domain.ImmutableProviderCommonData;
+import de.ii.xtraplatform.features.domain.MappingOperationResolver;
+import de.ii.xtraplatform.features.domain.ProviderData;
+import de.ii.xtraplatform.features.domain.SchemaFragmentResolver;
+import de.ii.xtraplatform.features.domain.SchemaReferenceResolver;
+import de.ii.xtraplatform.features.domain.TypesResolver;
+import de.ii.xtraplatform.features.domain.transform.DefaultRolesResolver;
+import de.ii.xtraplatform.features.domain.transform.FeatureRefEmbedder;
+import de.ii.xtraplatform.features.domain.transform.FeatureRefResolver;
+import de.ii.xtraplatform.features.domain.transform.ImplicitMappingResolver;
+import de.ii.xtraplatform.features.domain.transform.LabelTemplateResolver;
+import de.ii.xtraplatform.features.sql.domain.FeatureProviderSqlData;
+import de.ii.xtraplatform.features.sql.domain.FeatureProviderSqlData.DatasetChangeMode;
+import de.ii.xtraplatform.features.sql.domain.ImmutableConnectionInfoSql;
+import de.ii.xtraplatform.features.sql.domain.ImmutableDatasetChangeSettings;
+import de.ii.xtraplatform.features.sql.domain.ImmutableFeatureProviderSqlData.Builder;
+import de.ii.xtraplatform.features.sql.domain.ImmutablePoolSettings;
+import de.ii.xtraplatform.features.sql.domain.ImmutableQueryGeneratorSettings;
+import de.ii.xtraplatform.features.sql.domain.ImmutableSqlPathDefaults;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+@AutoBind
+@SuppressWarnings("PMD.TooManyMethods")
+public class FeatureProviderGeoParquetFactory
+    extends AbstractEntityFactory<FeatureProviderDataV2, FeatureProviderGeoParquet>
+    implements EntityFactory {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(FeatureProviderGeoParquetFactory.class);
+
+  private final Lazy<Set<SchemaFragmentResolver>> schemaResolvers;
+  private final boolean skipHydration;
+  private final Set<String> connectors;
+
+  @Inject
+  public FeatureProviderGeoParquetFactory(
+      Lazy<Set<SchemaFragmentResolver>> schemaResolvers,
+      ProviderGeoParquetFactoryAssisted providerGeoParquetFactoryAssisted) {
+    super(providerGeoParquetFactoryAssisted);
+    this.schemaResolvers = schemaResolvers;
+    this.skipHydration = false;
+    this.connectors = Set.of();
+  }
+
+  // for ldproxy-cfg
+  public FeatureProviderGeoParquetFactory() {
+    super(null);
+    this.schemaResolvers =
+        new Lazy<>() {
+          @Override
+          public Set<SchemaFragmentResolver> get() {
+            return Set.of();
+          }
+        };
+    this.skipHydration = true;
+    this.connectors = Set.of();
+  }
+
+  @Override
+  public String type() {
+    return ProviderData.ENTITY_TYPE;
+  }
+
+  @Override
+  public Optional<String> subType() {
+    return Optional.of(FeatureProviderGeoParquet.ENTITY_SUB_TYPE);
+  }
+
+  @Override
+  public Class<? extends PersistentEntity> entityClass() {
+    return FeatureProviderGeoParquet.class;
+  }
+
+  @Override
+  public EntityDataBuilder<FeatureProviderDataV2> dataBuilder() {
+    return new Builder()
+        .typeValidation(MODE.NONE)
+        .datasetChanges(
+            new ImmutableDatasetChangeSettings.Builder().mode(DatasetChangeMode.OFF).build())
+        .sourcePathDefaults(new ImmutableSqlPathDefaults.Builder().build())
+        .queryGeneration(new ImmutableQueryGeneratorSettings.Builder().build())
+        .connectionInfo(
+            new ImmutableConnectionInfoSql.Builder()
+                .database("")
+                //.dialect(SqlDbmsAdapterOras.ID)
+                .pool(
+                    new ImmutablePoolSettings.Builder()
+                        .maxConnections(-1)
+                        .minConnections(1)
+                        .idleTimeout("10m")
+                        .shared(false)
+                        .build())
+                .build());
+  }
+
+  @Override
+  public EntityDataBuilder<? extends EntityData> superDataBuilder() {
+    return new ImmutableProviderCommonData.Builder();
+  }
+
+  @Override
+  public EntityDataBuilder<FeatureProviderDataV2> emptyDataBuilder() {
+    return new Builder();
+  }
+
+  @Override
+  public EntityDataBuilder<? extends EntityData> emptySuperDataBuilder() {
+    return new ImmutableProviderCommonData.Builder();
+  }
+
+  @Override
+  public Class<? extends EntityData> dataClass() {
+    return FeatureProviderSqlData.class;
+  }
+
+  @Override
+  public Optional<AutoEntityFactory> auto() {
+    return Optional.empty();
+  }
+
+  @Override
+  public EntityData hydrateData(EntityData entityData) {
+    if (skipHydration) {
+      return entityData;
+    }
+
+    FeatureProviderSqlData data = (FeatureProviderSqlData) entityData;
+
+    try {
+      List<TypesResolver> resolvers =
+          List.of(
+              new SchemaReferenceResolver(data, schemaResolvers),
+              new MappingOperationResolver(true),
+              new FeatureRefEmbedder(data.getId()),
+              new FeatureRefResolver(connectors),
+              new ImplicitMappingResolver(),
+              new ConstantsResolver(),
+              new LabelTemplateResolver(data.getLabelTemplate()),
+              new MappingOperationResolver(),
+              new DefaultRolesResolver());
+
+      for (TypesResolver resolver : resolvers) {
+        data = applyTypesResolver(data, resolver);
+      }
+
+      return data;
+
+    } catch (Throwable e) {
+      LogContext.error(
+          LOGGER, e, "Feature provider with id '{}' could not be started", data.getId());
+    }
+
+    throw new IllegalStateException();
+  }
+
+  @Override
+  public Map<String, String> getListEntryKeys() {
+    return Map.of("extensions", "type");
+  }
+
+  private FeatureProviderSqlData applyTypesResolver(
+      FeatureProviderSqlData data, TypesResolver resolver) {
+    Map<String, FeatureSchema> types = data.getTypes();
+    int rounds = 0;
+
+    while (resolver.needsResolving(types)) {
+      types = resolver.resolve(types);
+      rounds++;
+      if (rounds >= resolver.maxRounds()) {
+        resolver.maxRoundsWarning().ifPresent(LOGGER::warn);
+        break;
+      }
+    }
+
+    if (rounds > 0) {
+      return new Builder().from(data).types(types).fragments(Map.of()).build();
+    }
+
+    return data;
+  }
+
+  @AssistedFactory
+  public interface ProviderGeoParquetFactoryAssisted
+      extends FactoryAssisted<FeatureProviderDataV2, FeatureProviderGeoParquet> {
+    @Override
+    FeatureProviderGeoParquet create(FeatureProviderDataV2 data);
+  }
+}
